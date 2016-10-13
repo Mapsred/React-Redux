3a705b8ceccb20b5e9939a58c041f6facb2b5cc2
maps_red <maps_red@yahoo.fr>
2016-10-13 16:38:57 +0200

    Fix second exercice

--- index.android.js
+++ index.android.js
@@ -1,6 +1,7 @@
 /**
  * Created by maps_red on 11/10/16.
  */
+//noinspection JSUnresolvedVariable
 import React, {Component} from 'react';
 import {AppRegistry, Navigator, Text, View} from 'react-native';
 
@@ -18,23 +19,21 @@ class AwesomeProject extends Component {
                                  index: nextIndex,
                              });
                          }}
-                    // Function to call to go back to the previous scene
                          onBack={() => {
-
                              if (route.index == 3) {
                                  navigator.push({
                                      title: 'Scene ' + 1,
                                      index: 1,
                                  });
-                             }else if (route.index > 0) {
+                             } else if (route.index > 0) {
                                  navigator.pop();
                              }
-                         }}
-                />
-            }
-            />
+                         }}/>
+            }/>
         )
     }
 }
 
-AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
+AppRegistry.registerComponent('AwesomeProject', () => {
+    return AwesomeProject;
+});
