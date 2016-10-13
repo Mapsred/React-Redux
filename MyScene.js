0df6fa957f4de2642b361d577ff9be593ba83487
maps_red <maps_red@yahoo.fr>
2016-10-13 16:36:41 +0200

    Add second exercice

---
+++ MyScene.js
@@ -0,0 +1,26 @@
+/**
+ * Created by maps_red on 13/10/16.
+ */
+import React, { Component, PropTypes } from 'react';
+import { View, Text, TouchableHighlight } from 'react-native';
+
+export default class MyScene extends Component {
+    static propTypes = {
+        title: PropTypes.string.isRequired,
+        onForward: PropTypes.func.isRequired,
+        onBack: PropTypes.func.isRequired,
+    };
+    render() {
+        return (
+            <View refreshing>
+                <Text>Current Scene: { this.props.title }</Text>
+                <TouchableHighlight onPress={this.props.onForward}>
+                    <Text>Tap me to load the next scene</Text>
+                </TouchableHighlight>
+                <TouchableHighlight onPress={this.props.onBack}>
+                    <Text>Tap me to go back</Text>
+                </TouchableHighlight>
+            </View>
+        )
+    }
+}
--- index.android.js
+++ index.android.js
@@ -1,9 +1,40 @@
 /**
  * Created by maps_red on 11/10/16.
  */
-import React, { AppRegistry } from 'react-native';
-import App from './app/containers/app';
+import React, {Component} from 'react';
+import {AppRegistry, Navigator, Text, View} from 'react-native';
 
-AppRegistry.registerComponent('AwesomeProject', () => {
-    return App;
-});
+import MyScene from './MyScene';
+
+class AwesomeProject extends Component {
+    render() {
+        return (
+            <Navigator initialRoute={{title: 'My Initial Scene', index: 0}} renderScene={(route, navigator) =>
+                <MyScene title={route.title}
+                         onForward={ () => {
+                             const nextIndex = route.index + 1;
+                             navigator.push({
+                                 title: 'Scene ' + nextIndex,
+                                 index: nextIndex,
+                             });
+                         }}
+                    // Function to call to go back to the previous scene
+                         onBack={() => {
+
+                             if (route.index == 3) {
+                                 navigator.push({
+                                     title: 'Scene ' + 1,
+                                     index: 1,
+                                 });
+                             }else if (route.index > 0) {
+                                 navigator.pop();
+                             }
+                         }}
+                />
+            }
+            />
+        )
+    }
+}
+
+AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
