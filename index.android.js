28e722ec6e836503f6b3b3e6f81bd567dd0ec2a4
maps_red <maps_red@yahoo.fr>
2016-10-26 15:19:05 +0200

    Add third exercice

--- index.android.js
+++ index.android.js
@@ -3,12 +3,53 @@
  */
 //noinspection JSUnresolvedVariable
 import React, {Component} from 'react';
-import {AppRegistry, Navigator, Text, View} from 'react-native';
+import {
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View,
+    TouchableOpacity,
+    AsyncStorage,
+    Navigator,
+} from 'react-native';
 
-import MyScene from './MyScene';
+const API_URL = "http://opendata.paris.fr/api/records/1.0/search/?dataset=stations-velib-disponibilites-en-temps-reel&facet=banking&facet=bonus&facet=status&facet=contract_name";
+
+class ApiClient {
+    constructor() {
+        this.cache = AsyncStorage
+    }
+
+    async get(url) {
+        const key = encodeURIComponent(url);
+        try {
+            let value = await this.cache.getItem(key);
+            if (value !== null) {
+                return Promise.resolve({data: JSON.parse(value), from: "Cache"});
+            }else {
+                return fetch(url)
+                    .then(response => response.json())
+                    .then((json) => {
+                        this.cache.setItem(key, JSON.stringify(json));
+                        return {data: json, from: 'API'};
+                    })
+            }
+        }catch (error) {
+            // Error retrieving data
+        }
+    }
+
+    clearCache() {
+        AsyncStorage.clear();
+    }
+}
 
 class AwesomeProject extends Component {
+    constructor() {
+        super();
+    }
     render() {
+        const data = JSON.stringify(this.state.data);
         return (
             <Navigator initialRoute={{title: 'My Initial Scene', index: 0}} renderScene={(route, navigator) =>
                 <MyScene title={route.title}
