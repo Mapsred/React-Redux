a6091cd898cf1561739f34a54dff06b4e4f3b106
maps_red <maps_red@yahoo.fr>
2016-10-26 16:21:29 +0200

    Add TP

---
+++ TP/TP 01.js
@@ -0,0 +1,9 @@
+/**
+ * Created by maps_red on 11/10/16.
+ */
+import React, { AppRegistry } from 'react-native';
+import App from '../app/containers/app';
+
+AppRegistry.registerComponent('AwesomeProject', () => {
+    return App;
+});
